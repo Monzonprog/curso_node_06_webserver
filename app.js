b437ed181942fdf869cb2f5042bcaf1ceb64cfaa
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2023-08-16 22:10:02 +0200

    Includes new static web

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const port = 8080;
 // Servir contenido estático
 app.use(express.static('public'));
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hello World in new route');
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
 });
 
 app.get('*', (req, res) => {
